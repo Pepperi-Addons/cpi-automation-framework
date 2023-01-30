e9e0409cc47769722b977e6107b7f70a25e3433c
Saar Guttman <saar.g@pepperi.com>
2023-01-30 09:27:35 +0200

    Remove redundant code

--- server-side/installation.ts
+++ server-side/installation.ts
@@ -9,26 +9,16 @@ The error Message is important! it will be written in the audit log and help the
 */
 
 import { Client, Request } from '@pepperi-addons/debug-server';
-import { AddonDataScheme, PapiClient, Relation } from '@pepperi-addons/papi-sdk';
-import { ADDON_BLOCK_NAME, SCHEMA_NAME } from 'shared-cpi-automation';
-import {AddonUUID} from '../addon.config.json'
+import { AddonDataScheme, PapiClient } from '@pepperi-addons/papi-sdk';
+import { SCHEMA_NAME } from 'shared-cpi-automation';
+import { AddonUUID } from '../addon.config.json'
 
 export async function install(client: Client, request: Request): Promise<any> 
 {
 	const resultObject = {success: true, resultObject: {}};
-	let relation: Relation | undefined = undefined;
 	let schema: AddonDataScheme | undefined = undefined;
 
 	const papiClient: PapiClient = createPapiClient(client);
-
-	// try
-	// {
-	// 	relation = await upsertAddonBlockRelation(papiClient);
-	// }
-	// catch(error)
-	// {
-	// 	handleError(error, "relation", resultObject);
-	// }
 
 	try
 	{
@@ -36,10 +26,15 @@ export async function install(client: Client, request: Request): Promise<any>
 	}
 	catch(error)
 	{
-		handleError(error, "schema", resultObject);
+		const errorMessage = error instanceof Error ? error.message : `An unknown error occurred trying to upsert a schema.`;
+		console.error(errorMessage);
+		resultObject.success = false;
 	}
 	
-	resultObject.resultObject = { CreatedSchema: schema};
+	if(resultObject.success)
+	{
+		resultObject.resultObject = { CreatedSchema: schema};
+	}
 
 	return resultObject;
 }
@@ -52,14 +47,6 @@ function createPapiClient(client: Client): PapiClient {
 		addonSecretKey: client.AddonSecretKey,
 		addonUUID: client.AddonUUID,
 	})
-}
-
-function handleError(error: unknown, resourceName: string, resultObject: any): void 
-{
-	const errorMessage = error instanceof Error ? error.message : `An unknown error occurred trying to upsert a ${resourceName}.`;
-		console.error(errorMessage);
-		resultObject.success = false;
-		resultObject.errorMessage = resultObject.errorMessage ? `${resultObject.errorMessage}\n${errorMessage}`: errorMessage;
 }
 
 export async function uninstall(client: Client, request: Request): Promise<any> 
@@ -75,25 +62,6 @@ export async function upgrade(client: Client, request: Request): Promise<any>
 export async function downgrade(client: Client, request: Request): Promise<any> 
 {
 	return {success:true,resultObject:{}}
-}
-
-async function upsertAddonBlockRelation(papiClient: PapiClient): Promise<Relation>
-{
-	const addonBlockRelation: Relation = {
-		RelationName: "AddonBlock",
-		Name: ADDON_BLOCK_NAME,
-		Description: "CPI Automation Framework addon block",
-		Type: "NgComponent",
-		SubType: "NG14",
-		AddonUUID: AddonUUID,
-		AddonRelativeURL: "",
-		ComponentName: "",
-		ModuleName: "",
-		ElementsModule: "",
-		ElementName: "",
-	}; 
-	
-	return await papiClient.addons.data.relations.upsert(addonBlockRelation);
 }
 
 async function createUserEventsSchema(papiClient: PapiClient): Promise <AddonDataScheme>
