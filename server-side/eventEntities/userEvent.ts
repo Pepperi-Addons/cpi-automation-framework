64fdbcf0bf149bc2917eb11703b3324ce71a69a7
Saar Guttman <saar.g@pepperi.com>
2023-02-05 19:43:20 +0200

    Handle User Events

--- server-side/eventEntities/userEvent.ts
+++ server-side/eventEntities/userEvent.ts
@@ -1,3 +1,4 @@
+import { ADDON_BLOCK_NAME } from "shared-cpi-automation";
 import { EventResponseUserEvent, UserEventExecutionResult } from "../constants";
 import { EventsService } from "../services/events.service";
 import {EventResult}  from "./index";
@@ -8,11 +9,43 @@ export class UserEvent extends EventResult
 	constructor(protected eventsService: EventsService, protected data: EventResponseUserEvent)
 	{
 		super(eventsService, data);
+		this.validateUserEvent(data);
+	}
+
+	/**
+	 * Validate the AddonBlockName and Description belong to a UserEvent.
+	 * @param data {EventResponseUserEvent} - The data to validate.
+	 */
+	protected validateUserEvent(data: EventResponseUserEvent) {
+		let errorMessage = "";
+		if (data.Value.Data.AddonBlockName !== ADDON_BLOCK_NAME) 
+		{
+			errorMessage = `The AddonBlockName is not ${ADDON_BLOCK_NAME}.`;
+		}
+		else if (data.Value.Data.Title !== ADDON_BLOCK_NAME)
+		{
+			errorMessage = `The Title is not ${ADDON_BLOCK_NAME}.`;
+		}
+
+		if (errorMessage !== "")
+		{
+			throw new Error(`Failed to create an instance of a User Event. ${errorMessage}\nGot the following data: ${JSON.stringify(data)}`);
+		}
 	}
 	
 	get eventData() 
 	{
-		return this.data.Value.Data
+		return this.data.Value.Data.HostObject.pageParams.userEventData
+	}
+
+	get userEventName() 
+	{
+		return this.data.Value.Data.HostObject.pageParams.userEventName
+	}
+
+	get eventType() 
+	{
+		return "UserEvent";
 	}
 
 	/**
